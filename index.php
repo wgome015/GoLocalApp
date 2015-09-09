617771911f12e8cc5dde310c9179a62bc142dc42
Wilfredo G Gomez <wgome015@fiu.edu>
2015-09-09 11:03:01 -0400

    index

--- index.php
+++ index.php
@@ -4,6 +4,26 @@
 </header>
 <body>
 
+  <!-- Create the form -->
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+     User Type: <input type="radio" name="userType" value="registeredstaff">Staff
+              <input type="radio" name="userType" value="registeredcompany">Company
+    <br><br>
+    Username: <input type="text" name="username" value="<?php echo $username; ?>">
+      <span class="error">*<?php echo $usernameError; ?></span><br><br>
+    <span class="error"><?php echo $passwordMatchError; ?></span>
+    Password: <input type="password" name="password">
+      <span class="error">*<?php echo $passwordError; ?></span><br><br>
+    Confirm Password: <input type="password" name="confirmPass">
+      <span class="error">*<?php echo $passwordError; ?></span><br><br>
+    <span class="error"><?php echo $emailMatchError; ?></span>
+    Email: <input type="email" name="email">
+      <span class="error">*<?php echo $emailError; ?></span><br><br>
+    Confirm Email: <input type="email" name="confirmEmail">
+      <span class="error">*<?php echo $emailError; ?></span><br><br>
+    <input type="submit" value="Submit">
+  </form>
+
   <?php
   //define variables used in the form
   $username;
@@ -11,76 +31,81 @@
   $confirmPass;
   $email;
   $confirmEmail;
-  $fname;
-  $middleI;
-  $lname;
-  $gender;
   $usernameError = "";
   $passwordError = "";
   $emailError = "";
   $passwordMatchError = "";
   $emailMatchError = "";
+  $userType = "";
+  $userTypeError = "";
 
   $db = mysqli_connect("localhost", "root", "root", "golocalapp");
   if( !$db )
     echo "Unable to connect to MySQL.".PHP_EOL;
-
-  if( $_SERVER["REQUEST_METHOD"] == "POST" )
+  else
   {
-    if( empty($_POST["username"]) )
-      $usernameError = "Username is required";
-    else
+    if( $_SERVER["REQUEST_METHOD"] == "POST" )
     {
-      $username = test_input($_POST["username"]);
-    }
+      if( empty($_POST["userType"]) )
+        $userTypeError = "This field is required";
+      else
+        $userType = $_POST["userType"];
 
-    if( empty($_POST["password"]) || empty($_POST["confirmPass"]) )
-      $passwordError = "Passowrd is required";
-    else
-    {
-      if( $_POST["password"] != $_POST["confirmPass"] )
-        $passwordMatchError = "The passwords don't match<br>";
+      if( empty($_POST["username"]) )
+        $usernameError = "Username is required";
       else
       {
-        // $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        // $query = "";
-        // $result = mysqli_query($db, $query);
-        // $row = mysqli_fetch_array( $result, MYSQLI_ASSOC );
-
-        // if( password_verify( $password, $row["password"]) )
-        // {
-        //   echo "SUCCESS";
-        // }
-        // else
-        // {
-        //   echo "WONG PASSWORD";
-        // }
+        $username = test_input($_POST["username"]);
       }
-    }
 
-    if( empty($_POST["email"]) || empty($_POST["confirmEmail"]) )
-      $emailError = "Email is required";
-    else
-    {
-      if( $_POST["email"] != $_POST["confirmEmail"] )
-        $emailMatchError = "Emails do not match<br>";
+      if( empty($_POST["password"]) || empty($_POST["confirmPass"]) )
+        $passwordError = "Passowrd is required";
       else
       {
-        $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        $email = $_POST["email"];
-        $query = "INSERT INTO registeredstaff (peopleID, username, password, email)
-                  VALUES ( '0', '$username', '$passwordHashed', '$email' )";
-        $result = mysqli_query($db, $query);
+        if( $_POST["password"] != $_POST["confirmPass"] )
+          $passwordMatchError = "The passwords don't match<br>";
+        else
+        {
+          // $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
+          // $query = "";
+          // $result = mysqli_query($db, $query);
+          // $row = mysqli_fetch_array( $result, MYSQLI_ASSOC );
 
-        $to = $email;
-        $subject = "GoLocalApp email verification";
-        $message = "Thanks for signing up!";
-        $from = "noreply@golocalpromos.com";
-        mail( $to, $subject, $message );
-
+          // if( password_verify( $password, $row["password"]) )
+          // {
+          //   echo "SUCCESS";
+          // }
+          // else
+          // {
+          //   echo "WONG PASSWORD";
+          // }
+        }
       }
-    }
 
+      if( empty($_POST["email"]) || empty($_POST["confirmEmail"]) )
+        $emailError = "Email is required";
+      else
+      {
+        if( $_POST["email"] != $_POST["confirmEmail"] )
+          $emailMatchError = "Emails do not match<br>";
+        else
+        {
+          $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
+          $email = $_POST["email"];
+
+          $query = "INSERT INTO $userType (username, password, email)
+                    VALUES ( '$username', '$passwordHashed', '$email' )";          
+          $result = mysqli_query($db, $query);
+
+          $to = $email;
+          $subject = "GoLocalApp email verification";
+          $message = "Thanks for signing up!";
+          $from = "noreply@golocalpromos.com";
+          mail( $to, $subject, $message );
+        }
+      }
+
+    }
   }
 
   function test_input($data)
@@ -92,26 +117,6 @@
   }
   
   ?>
-
-  <!-- Create the form -->
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  	Username: <input type="text" name="username" value="<?php echo $username; ?>">
-      <span class="error">*<?php echo $usernameError; ?></span><br><br>
-    <span class="error"><?php echo $passwordMatchError; ?></span>
-  	Password: <input type="password" name="password">
-      <span class="error">*<?php echo $passwordError; ?></span><br><br>
-  	Confirm Password: <input type="password" name="confirmPass">
-      <span class="error">*<?php echo $passwordError; ?></span><br><br>
-    <span class="error"><?php echo $emailMatchError; ?></span>
-  	Email: <input type="email" name="email"><br><br>
-  	Confirm Email: <input type="email" name="confirmEmail"><br><br>
-  	Name: <input type="text" name="fname"><br><br>
-  	Middle Initial: <input type="text" name="middleI"><br><br>
-  	Last Name: <input type"text" name="lname"><br><br>
-  	Gender: <input type="radio" name="gender" value="female">Female
-  			<input type="radio" name="gender" value="male">Male
-  	<br><br><input type="submit" value="Submit">
-  </form>
   	
 </body>
 </html>
